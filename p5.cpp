57337ccbea3076f869198ed827f6b9166b9ce15d
tanusumra <141604389+tanusumra@users.noreply.github.com>
2023-08-23 19:13:22 -0700

    Add files via upload

---
+++ p5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+	
+
+	float pi=3.14285;
+	
+	cout<<"tanya A. sumra"<<endl;
+	cout<<"220130318009"<<endl;
+	cout<<"value of pi is::"<<pi<<endl;
+	cout<<"aafter applying precision::"<<setprecision(3)<<pi;
+}
