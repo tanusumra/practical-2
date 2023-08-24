096038008df70eb8383986361e2f7b55ddaecb40
tanusumra <141604389+tanusumra@users.noreply.github.com>
2023-08-23 19:39:03 -0700

    Add files via upload

---
+++ p7.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+enum AND{FALSE,TRUE};
+int main()
+{
+	cout<<"tanya A. sumra"<<endl;
+	cout<<"220130318009"<<endl;
+	cout<<"A"<<"\t"<<"b"<<"\t"<<"Y"<<endl;
+	cout<<FALSE<<"\t"<<FALSE<<"\t"<<FALSE*FALSE<<endl;
+	cout<<FALSE<<"\t"<<TRUE<<"\t"<<FALSE*TRUE<<endl;
+	cout<<TRUE<<"\t"<<FALSE<<"\t"<<TRUE*FALSE<<endl;
+	cout<<TRUE<<"\t"<<TRUE<<"\t"<<TRUE*TRUE<<endl;
+	return 0;
+}
+
