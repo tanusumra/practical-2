9a4629640b3e728d7c8d0ca1c11a841863bb5e1e
tanusumra <141604389+tanusumra@users.noreply.github.com>
2023-08-23 19:12:42 -0700

    Add files via upload

---
+++ p3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class temp
+{
+	public:
+		void display()
+{
+	cout<<"hello welcome to DSU"<<endl;
+}
+};
+int main()
+{
+	temp t1;
+	t1.display();
+	return 0;
+}
+
